b7788a176bc6b2ae45c8faf0985d8bf7ae9c718a
Steve Sinnwell <steve@kopfwelt.com>
2017-01-04 15:52:09 +0100

    Release 0.0.3

--- docset.js
+++ docset.js
@@ -1,60 +1,84 @@
-var DocSetGenerator = require('docset-generator').DocSetGenerator;
-var generator = new DocSetGenerator({
-    destination: "./docset",
-    name: "Kern",
-    identifier: "kern",
-    documentation: "./documentation",
-    // icon: "./styleguide/icon@2x.png",
-    enableJavascript: true,
-    entries:[
-    {
-    	name:'colors',
-    	type:'Style',
-    	path:'index.html'
-    }
-    ]
-
+// var DocSetGenerator = require('docset-generator').DocSetGenerator;
+// var generator = new DocSetGenerator({
+//     destination: "./docset",
+//     name: "Kern",
+//     identifier: "kern",
+//     documentation: "./documentation",
+//     // icon: "./styleguide/icon@2x.png",
+//     enableJavascript: true,
+//     entries:[
 //     {
-//     	name:'Typography',
+//     	name:'colors',
 //     	type:'Style',
-//     	path:'index.html#typography'
-//     },
+//     	path:'index.html'
+//     }
+//     ]
 
-//     {
-//     	name:'Grids',
-//     	type:'Style',
-//     	path:'index.html#grids-gb'
-//     },
+// //     {
+// //     	name:'Typography',
+// //     	type:'Style',
+// //     	path:'index.html#typography'
+// //     },
 
-// // Components
-//     {
-//     	name:'Burger',
-//     	type:'Component',
-//     	path:'components.html#burger'
-//     },
+// //     {
+// //     	name:'Grids',
+// //     	type:'Style',
+// //     	path:'index.html#grids-gb'
+// //     },
 
-//     {
-//     	name:'Newsletter',
-//     	type:'Component',
-//     	path:'components.html#newsletter'
-//     },
+// // // Components
+// //     {
+// //     	name:'Burger',
+// //     	type:'Component',
+// //     	path:'components.html#burger'
+// //     },
 
-//     {
-//     	name:'Gallery',
-//     	type:'Component',
-//     	path:'components.html#gallery'
-//     },
-// // Modules
-//     {
-//     	name:'Bar',
-//     	type:'Modules',
-//     	path:'modules.html#bar'
-//     },
-//     {
-//     	name:'Navigation',
-//     	type:'Modules',
-//     	path:'modules.html#navigation'
-//     }]
-  }
-);
-generator.create();
+// //     {
+// //     	name:'Newsletter',
+// //     	type:'Component',
+// //     	path:'components.html#newsletter'
+// //     },
+
+// //     {
+// //     	name:'Gallery',
+// //     	type:'Component',
+// //     	path:'components.html#gallery'
+// //     },
+// // // Modules
+// //     {
+// //     	name:'Bar',
+// //     	type:'Modules',
+// //     	path:'modules.html#bar'
+// //     },
+// //     {
+// //     	name:'Navigation',
+// //     	type:'Modules',
+// //     	path:'modules.html#navigation'
+// //     }]
+//   }
+// );
+// generator.create();
+
+
+var git = require('git-rev');
+git.tag(function (tag) {
+    console.log('tag', tag);
+
+    var xmlString = '';
+
+    var fs = require('fs');
+    fs.writeFile("./docset/kern.xml", "<entry><version>"+ tag +"</version><ios_version>1</ios_version><url>http://docsets.kopfwelt.com/docsets/kern.tgz</url></entry>", function(err) {
+        if(err) {
+            return console.log(err);
+        }
+
+        console.log("The file was saved!");
+    }); 
+    /*
+   
+    */
+    // var originalContents = String(file.contents);
+    // var newContents = new Buffer(originalContents + '<!-- tag:' + tag + ' -->');
+    // file.contents = newContents;
+    // callback(null, file);
+});
