61b8a3a8aff6165d28924dc6a73665024eeda801
Samuel <rott.samuel@gmail.com>
2017-01-05 16:11:30 +0100

    working test but still need to clean css

--- test/theme/default-theme.js
+++ test/theme/default-theme.js
@@ -4,13 +4,13 @@ import preprocessor from 'suitcss-preprocessor';
 import {plugins} from '../helpers/config';
 import test from 'ava';
 
-test('elements.css should provide a working css', async t => {
-    let css = await pify(fs.readFile)('./css/theme/default/elements.css', 'utf8');
-    const fixture = await pify(fs.readFile)('./test/fixtures/theme/default/elements.css', 'utf8');
+test('default-theme.css should provide a working css', async t => {
+    let css = await pify(fs.readFile)('./css/theme/default/theme-default.css', 'utf8');
+    const fixture = await pify(fs.readFile)('./test/fixtures/theme/default/theme-default.css', 'utf8');
 
 
     const result = await preprocessor(css, {
-      root: '/css',
+      root: './css',
       minify: true,
       use: plugins
     });
