eae9dfdfc9143b4e491bbaf1870801b46778130c
Samuel <rott.samuel@gmail.com>
2017-01-05 13:11:19 +0100

    solve merge conflict

--- test/helpers/config.js
+++ test/helpers/config.js
@@ -1,3 +1,4 @@
 const plugins = ['postcss-nested', 'postcss-mixins', 'postcss-conditionals', 'postcss-each', 'postcss-for', 'postcss-calc', 'postcss-import', 'cssnext'];
 
+
 module.exports = {plugins};
