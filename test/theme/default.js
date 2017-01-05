02a6355c862722560afc7128beff5524fd8e06ca
Samuel <rott.samuel@gmail.com>
2017-01-05 13:10:31 +0100

    create test for theme/elements

---
+++ test/theme/default.js
@@ -0,0 +1,19 @@
+import fs from 'fs';
+import pify from 'pify';
+import preprocessor from 'suitcss-preprocessor';
+import {plugins} from '../helpers/config';
+import test from 'ava';
+
+test('elements.css should provide a working css', async t => {
+    let css = await pify(fs.readFile)('./css/theme/default/elements.css', 'utf8');
+    const fixture = await pify(fs.readFile)('./test/fixtures/theme/default/elements.css', 'utf8');
+
+
+    const result = await preprocessor(css, {
+      root: './css/theme/default/elements.css',
+      minify: true,
+      use: plugins
+    });
+
+    t.deepEqual(result.css, fixture);
+});
