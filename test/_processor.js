6eb8f7c4caadb01ea639bde8ebaa0eb02298b478
Steve Sinnwell <steve@kopfwelt.com>
2017-01-10 14:23:12 +0100

    test

--- test/_processor.js
+++ test/_processor.js
@@ -1,7 +1,26 @@
 import fs from 'fs';
 import pify from 'pify';
 import preprocessor from 'suitcss-preprocessor';
-import {options} from './config';
+
+const plugins = ['postcss-mixins', 'postcss-nested', 'postcss-for', 'postcss-calc'];
+const options = {
+  root: './css',
+  minify: true,
+  use: plugins,
+  autoprefixer: {
+    // disable
+    browsers: '',
+    cascade: true
+  },
+  cssnano: {
+    calc: false,
+    autoprefixer: false,
+    mergeRules: false,
+    safe: true,
+    discardComments: true,
+    core: false
+  }
+};
 
 const run = async (t, input, output) => {
   const css = await pify(fs.readFile)(input, 'utf8');
--- test/helpers/config.js
+++ test/helpers/config.js
@@ -1,5 +1,22 @@
 // const plugins = ['postcss-import', 'postcss-nested', 'postcss-mixins', 'postcss-conditionals', 'postcss-each', 'postcss-for', 'postcss-calc', 'cssnext'];
 const plugins = ['postcss-mixins', 'postcss-nested', 'postcss-for', 'postcss-calc'];
+const options = {
+  root: './css',
+  minify: true,
+  use: plugins,
+  autoprefixer: {
+    // disable
+    browsers: '',
+    cascade: true
+  },
+  cssnano: {
+    calc: false,
+    autoprefixer: false,
+    mergeRules: false,
+    safe: true,
+    discardComments: true,
+    core: false
+  }
+};
 
-
-module.exports = {plugins};
+module.exports = {plugins, options};
