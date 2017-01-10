3664a7efe23ea99cca696f11737d7a6398dfe8f7
Steve Sinnwell <steve@kopfwelt.com>
2017-01-10 22:50:45 +0100

    [refactor] tests setup

--- test/postcss.js
+++ test/postcss.js
@@ -1,21 +1,6 @@
-import fs from 'fs';
-import pify from 'pify';
-import postcss from 'postcss';
-import {plugins} from './helpers/config';
 import test from 'ava';
+import {run} from './helpers/processor';
 
-// postcss plugins
-import preprocessor from 'suitcss-preprocessor';
-
-test('Testing Kern framework', async t => {
-    const css = await pify(fs.readFile)('./css/index.css', 'utf8');
-    const fixture = await pify(fs.readFile)('./test/fixtures/kern.css', 'utf8');
-    
-    const result = await preprocessor(css, {
-      root: './css',
-      minify: true,
-      use: plugins
-    });
-
-    t.is(result.css, fixture);
+test('run test', t => {
+	run(t, './css/kern.css', './test/fixtures/kern.css');
 });
