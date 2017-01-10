0b242d7875b9b480f3247d0dba143549b6b4b006
Henrik Wirth <henrik@kopfwelt.com>
2017-01-10 14:44:32 +0100

    [add] accordion js & delete colors.css import

---
+++ css/components/accordion/accordion.js
@@ -0,0 +1,24 @@
+const $ = require('jquery');
+
+export default class Accordion {
+	constructor() {
+		// super();
+		this._actions();
+	}
+	// target dt element in faq minor
+	// on click toggle class is-active to dl parent
+
+	onFaqClick(event) {
+		const accordionParents = $('.Accordion-child');
+		// const accordionTargets = $ ('.Accordion-subtitle');
+		// first remove '.is-active' on all of them while ignoring the one clicked
+		accordionParents.not(event.currentTarget).removeClass('is-active');
+		// then add it on the current target
+		$(event.currentTarget).toggleClass('is-active');
+	}
+
+	_actions() {
+		const accordionParents = $('.Accordion-child');
+		faqParents.on('click', this.onFaqClick.bind(this));
+	}
+}
