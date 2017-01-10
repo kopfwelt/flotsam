dad4c285044825a2d1b24ecc5d2ca608fafe08a1
Henrik Wirth <henrik@kopfwelt.com>
2017-01-10 12:23:06 +0100

    [add] test for rtl.css & [refactor] rtl.css to work with postcss

--- test/tools/helpers.js
+++ test/tools/helpers.js
@@ -13,10 +13,34 @@ test('animations.css should provide a working mixin', async t => {
 @mixin iterative-delay .default;
 @mixin iterative-delay .parameter, 4, 0.2s;
 `;
-    
+
     const result = await preprocessor(css, {
       root: './css',
       minify: true,
+      use: plugins
+    });
+
+    t.deepEqual(result.css, fixture);
+});
+
+
+test.only('rtl.css should provide a working mixin', async t => {
+    let css = await pify(fs.readFile)('./css/tools/helpers/rtl.css', 'utf8');
+    const fixture = await pify(fs.readFile)('./test/fixtures/tools/helpers/rtl.css', 'utf8');
+
+    // use mixin
+    css = `${css}
+.TestClass {
+  margin: 0 0 0 20px;
+  @mixin rtl {
+    margin: 0 20px 0 0;
+  }
+}
+`;
+
+    const result = await preprocessor(css, {
+      root: './css',
+      minify: false,
       use: plugins
     });
 
