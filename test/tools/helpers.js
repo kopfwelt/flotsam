66c25beb7f97918d98e63611387a297877d5b15c
Steve Sinnwell <steve@kopfwelt.com>
2017-01-11 10:54:26 +0100

    [fix] test file paths

--- test/tools/helpers.js
+++ test/tools/helpers.js
@@ -2,13 +2,13 @@ import test from 'ava';
 import {run} from '../helpers/processor';
 
 test('test.css', async t => {
-  await run(t, './test/fixtures/tools/helpers/test.css', './test/fixtures/tools/helpers/test.expected.css');
+  await run(t, 'test/fixtures/tools/helpers/test.css', 'test/fixtures/tools/helpers/test.expected.css');
 });
 
 test('animations.css', async t => {
-  await run(t, './test/fixtures/tools/helpers/animations.css', './test/fixtures/tools/helpers/animations.expected.css');
+  await run(t, 'test/fixtures/tools/helpers/animations.css', 'test/fixtures/tools/helpers/animations.expected.css');
 });
 
 test('rtl.css should provide a working mixin', async t => {
-  await run(t, './test/fixtures/tools/helpers/rtl.css', './test/fixtures/tools/helpers/rtl.expected.css');
+  await run(t, 'test/fixtures/tools/helpers/rtl.css', 'test/fixtures/tools/helpers/rtl.expected.css');
 });
