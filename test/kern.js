326dfd602b6d44831c144223c06dcd8fb1c02078
Steve Sinnwell <steve@kopfwelt.com>
2017-11-20 21:13:33 +0100

    [fix](Test) Change path to src

--- test/kern.js
+++ test/kern.js
@@ -2,5 +2,5 @@ import test from 'ava';
 import {run} from './helpers/processor';
 
 test('run test', async t => {
-	await run(t, './css/kern.css', './test/fixtures/kern.css');
+	await run(t, './src/kern.css', './test/fixtures/kern.css');
 });
